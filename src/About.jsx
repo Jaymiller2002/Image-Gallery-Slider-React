23b5e09c0a3f444af9acc17e370302b8b8b26b50
Jaymiller2002 <jay.miller02@icloud.com>
2024-05-05 16:38:05 -0400

    image slider

--- src/About.jsx
+++ src/About.jsx
@@ -1,13 +1,28 @@
 import { Link } from "react-router-dom"
+import React, { useState } from 'react'
+import './App.css'
 
-function About() {
+const About = ({ images }) => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const prevImage = () => {
+    setCurrentImageIndex((prevIndex) => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
+  };
+
+  const nextImage = () => {
+    setCurrentImageIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
+  };
+
   return (
-    <div className="p-5">
-      <Link to='/'>{'<- Back'}</Link>
-      <h1>About Page</h1>
+    <div className="image-slider">
+      <img src={images[currentImageIndex]} alt={`Image ${currentImageIndex + 1}`} />
+
+      <div className="navigation">
+        <button onClick={prevImage}>Prev</button>
+        <button onClick={nextImage}>Next</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-
-export default About
+export default About;
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import { Link } from "react-router-dom"
-
 const Title = () => {
   return (
     <h1>
@@ -8,14 +6,33 @@ const Title = () => {
   )
 }
 
-function App() {
+
+import React from 'react';
+import About from './About';
+import './App.css'; // Assume you have a CSS file for styling
+
+const App = () => {
+  const images = [
+    'image1.jpg',
+    'image2.jpg',
+    'image3.jpg',
+    'image4.jpg',
+    'image5.jpg',
+    'image6.jpg',
+    'image7.jpg',
+    'image8.jpg',
+    'image9.jpg',
+    'image10.jpg',
+    'image11.jpg',
+    // Add more image URLs as needed
+  ];
+
   return (
-    <div className="p-5">
-      <Link to='/about'>About</Link>
-      <Title />
+    <div className="app-container">
+      <h1>Image Gallery Slider</h1>
+      <About images={images} />
     </div>
-  )
-}
+  );
+};
 
-
-export default App
+export default App;
