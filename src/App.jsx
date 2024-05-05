4b0dee235d4c33b2f7f57b198c470654260890f4
Jaymiller2002 <jay.miller02@icloud.com>
2024-05-05 17:13:41 -0400

    image gallery slider done

--- src/App.jsx
+++ src/App.jsx
@@ -10,27 +10,39 @@ const Title = () => {
 import React from 'react';
 import About from './About';
 import './App.css'; // Assume you have a CSS file for styling
+import Image1 from './assets/image1.jpg'
+import Image2 from './assets/image2.jpg'
+import Image3 from './assets/image3.jpg'
+import Image4 from './assets/image4.jpg'
+import Image5 from './assets/image5.jpg'
+import Image6 from './assets/image6.jpg'
+import Image7 from './assets/image7.jpg'
+import Image8 from './assets/image8.jpg'
+import Image9 from './assets/image9.jpg'
+import Image10 from './assets/image10.jpg'
+import Image11 from './assets/image11.jpg'
 
 const App = () => {
   const images = [
-    'image1.jpg',
-    'image2.jpg',
-    'image3.jpg',
-    'image4.jpg',
-    'image5.jpg',
-    'image6.jpg',
-    'image7.jpg',
-    'image8.jpg',
-    'image9.jpg',
-    'image10.jpg',
-    'image11.jpg',
+    Image1,
+    Image2,
+    Image3,
+    Image4,
+    Image5,
+    Image6,
+    Image7,
+    Image8,
+    Image9,
+    Image10,
+    Image11
     // Add more image URLs as needed
   ];
 
   return (
     <div className="app-container">
-      <h1>Image Gallery Slider</h1>
+      <div className="image-slider">
       <About images={images} />
+      </div>
     </div>
   );
 };
